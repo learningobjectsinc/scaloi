1bdb1cc0637fe92acf9a56eceed8a573af434c4f
Zack Powers <zpowers@learningobjects.com>
2017-04-03 14:54:24 -0400

    Effectful logging functions for NatTrans.

--- scaloiz/src/main/scala/scaloi/NatTrans.scala
+++ scaloiz/src/main/scala/scaloi/NatTrans.scala
@@ -2,7 +2,7 @@ package scaloi
 
 import scala.collection.mutable
 import scala.language.higherKinds
-import scalaz.{-\/, Coproduct, Free, Monad, \/-, ~>}
+import scalaz.{-\/, Coproduct, Free, Functor, Monad, MonadError, Show, \/-, ~>}
 
 /**
   * Some general Natural transformations
@@ -11,18 +11,44 @@ object NatTrans {
 
   /** Prints instances of F and G to stdOut.
     */
-  def printOp[F[_], G[_]](intp: F ~> G  ) = log(println(_))(intp)
+  def printOp[F[_], G[_]](intp: F ~> G) = log(println(_))(intp)
 
-  def log[F[_], G[_]](logger: String => Unit)(intp: F ~> G) = {
+  def log[F[_], G[_]](logger: String => Unit)(intp: F ~> G): F ~> G = {
     new (F ~> G) {
-      override def apply[A](fa: F[A]): G[A] = fa match {
-        case op =>
-          val g = intp(op)
-          logger(s"$intp: $op ~> $g")
-          g
+      override def apply[A](fa: F[A]): G[A] = {
+        val ga = intp(fa)
+        logger(s"$intp: $fa ~> $ga")
+        ga
       }
     }
   }
+
+  /**
+    * Log the result of the Effect G and it's input.
+    */
+  def logEff[F[_], G[_]](logger: String => Unit)(intp: F ~> G)(implicit G: Functor[G]) = new (F ~> G) {
+    override def apply[A](fa: F[A]): G[A] = {
+      val ga = intp(fa)
+      G.map(ga)({ a =>
+        logger(s"$fa ~> $a")
+        a
+      })
+    }
+  }
+
+  /**
+    * Log the result of the Effect G, any failures, and it's input.
+    */
+  def logErr[F[_], G[_], E: Show](logger: String => Unit)(intp: F ~> G)(implicit G: MonadError[G, E]): (F ~> G) =
+    new (F ~> G) {
+      override def apply[A](fa: F[A]): G[A] = {
+        val logA = logEff(logger)(intp)(G)(fa)
+        G.handleError(logA)({ err =>
+          logger(s"$fa ~> ${Show[E].shows(err)}")
+          G.raiseError(err)
+        })
+      }
+    }
 
   /**
     * This should work with foldMap, need some way to prove List[F[A]] is a monad.
