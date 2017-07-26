b23ccd88b22010fa88d0998b76bfcddffde08cb1
Merlin Hughes <merlin@learningobjects.com>
2017-07-26 10:08:22 -0400

    make functor ops free, allocation style

--- scaloi-core/src/main/scala/scaloi/syntax/FunctorOps.scala
+++ scaloi-core/src/main/scala/scaloi/syntax/FunctorOps.scala
@@ -2,23 +2,23 @@ package scaloi
 package syntax
 
 import scalaz.Functor
-import scala.language.implicitConversions
 
 /**
   * Enhancements on functors.
   * @param self the functor
-  * @param F the functor evidence
   * @tparam F the functor type
   * @tparam A the functed type
   */
-final class FunctorOps[F[_], A](val self: F[A])(implicit val F: Functor[F]) {
+final class FunctorOps[F[_], A](val self: F[A]) extends AnyVal {
   /** Apply a partial function to the values in this functor. Values for
     * which the partial function is undefined remain unchanged.
     * @param pf the partial function
+    * @param F the functor evidence
     * @tparam A1 the result type
     * @return the partially transformed functor
     */
-  @inline final def pfmap[A1 >: A](pf: PartialFunction[A, A1]): F[A1] =
+  @inline
+  final def pfmap[A1 >: A](pf: PartialFunction[A, A1])(implicit F: Functor[F]): F[A1] =
     F.map(self)(fa => pf.applyOrElse(fa, (a: A) => a))
 }
 
@@ -31,6 +31,7 @@ object FunctorOps extends ToFunctorOps
   * Implicit conversion for functor operations.
   */
 trait ToFunctorOps {
+  import language.implicitConversions
 
   /**
     * Implicit conversion from functor to the functor enhancements.
@@ -38,5 +39,6 @@ trait ToFunctorOps {
     * @tparam F the functor type
     * @tparam A the functed type
     */
-  implicit def toFunctorOps[F[_] : Functor, A](f: F[A]): FunctorOps[F, A] = new FunctorOps(f)
+  @inline
+  implicit final def toFunctorOps[F[_] : Functor, A](f: F[A]): FunctorOps[F, A] = new FunctorOps(f)
 }
