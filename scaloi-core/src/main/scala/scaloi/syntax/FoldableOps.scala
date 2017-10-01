8ce88189d85cd87fe36188bf061676820ab1b534
Merlin Hughes <merlin@learningobjects.com>
2017-10-01 00:23:51 -0400

    some fine things

---
+++ scaloi-core/src/main/scala/scaloi/syntax/FoldableOps.scala
@@ -0,0 +1,45 @@
+package scaloi
+package syntax
+
+import scala.language.implicitConversions
+import scalaz.Foldable
+
+/**
+  * Enhancements on foldable things.
+  * @param self the foldable thing
+  * @tparam F the foldable type
+  * @tparam A the folded type
+  */
+final class FoldableOps[F[_], A](val self: F[A]) extends AnyVal {
+  /**
+    * Apply a map to an optional value to the elements of this foldable, returning
+    * the first defined result.
+    *
+    * @param f the map function
+    * @param ev foldable evidence
+    * @tparam B the target type
+    * @return the optional value
+    */
+  @inline final def findMap[B](f: A => Option[B])(implicit ev: Foldable[F]): Option[B] =
+    ev.foldLeft(self, Option.empty[B])((o, a) => o.orElse(f(a)))
+}
+
+/**
+  * Foldable operations companion.
+  */
+object FoldableOps extends ToFoldableOps
+
+/**
+  * Implicit conversion for foldable operations.
+  */
+trait ToFoldableOps {
+
+  /**
+    * Implicit conversion from foldable to its enhancements.
+    * @param f: the foldable thing
+    * @tparam F the foldable type
+    * @tparam A the folded type
+    */
+  implicit def toFoldableOps[F[_] : Foldable, A](f: F[A]): FoldableOps[F, A] =
+    new FoldableOps(f)
+}
