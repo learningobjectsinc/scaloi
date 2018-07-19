5eb9434284df6a2c5f925a347fde0bbcb450a812
Keith Wooldridge <kwooldridge@learningobjects.com>
2018-07-19 12:03:09 -0400

    (0547087fb8fb9aa598727545b2c2b2fd7171a44c)

--- src/main/scala/scaloi/json/ArgoExtras.scala
+++ src/main/scala/scaloi/json/ArgoExtras.scala
@@ -1,6 +1,8 @@
 package scaloi
 package json
 
+import java.time.Instant
+
 import argonaut._
 import scalaz._
 import scalaz.std.stream._
@@ -15,16 +17,25 @@ object ArgoExtras {
 
   implicit val longKeyEncoder: EncodeJsonKey[Long] = EncodeJsonKey.from(_.toString)
 
+  implicit def instantCodec: CodecJson[Instant] = {
+    val encode: EncodeJson[Instant] = _.toString.asJson
+    val decode: DecodeJson[Instant] = _.as[String].map(Instant.parse)
+    CodecJson.derived(encode, decode)
+  }
+
   implicit def longMapCodec[V: EncodeJson: DecodeJson]: CodecJson[Map[Long, V]] = {
     def encode(m: Map[Long, V]) = m.mapKeys(_.toString).asJson
     def decode(hc: HCursor) = hc.as[Map[String, V]].flatMap { stringMap =>
       DecodeResult.fromDisjunction(hc.history) {
-        stringMap.toStream.traverseU {
-          case (keyStr, v) => keyStr.toLong_?.toSuccessNel(keyStr).map(_ -> v)
-        }.disjunction.bimap(
-          badKeys => s"unacceptable non-numeric keys: (${badKeys.intercalate(", ")})",
-          assocs  => assocs.toMap
-        )
+        stringMap.toStream
+          .traverseU {
+            case (keyStr, v) => keyStr.toLong_?.toSuccessNel(keyStr).map(_ -> v)
+          }
+          .disjunction
+          .bimap(
+            badKeys => s"unacceptable non-numeric keys: (${badKeys.intercalate(", ")})",
+            assocs => assocs.toMap
+          )
       }
     }
     CodecJson.derived(encode, decode)
--- src/test/scala/scaloi/json/ArgoExtrasTest.scala
+++ src/test/scala/scaloi/json/ArgoExtrasTest.scala
@@ -1,38 +1,53 @@
 package scaloi
 package json
 
+import java.time.Instant
+
 import org.scalatest.{EitherValues, FlatSpec, Matchers}
 import scaloi.test.ScaloiTest
 
-class ArgoExtrasTest
-  extends FlatSpec
-    with EitherValues
-    with Matchers
-    with ScaloiTest
-{
+class ArgoExtrasTest extends FlatSpec with EitherValues with Matchers with ScaloiTest {
 
   behaviour of "ArgoExtras"
-  import argonaut._, Argonaut._, ArgoExtras._
+  import ArgoExtras._
+  import argonaut._
+  import Argonaut._
 
   it should "encode lengthy maps" in {
-    Map(1L -> "one").asJson.nospaces should equal ("""{"1":"one"}""")
-    Map(2L -> 22222).asJson.nospaces should equal ("""{"2":22222}""")
+    Map(1L -> "one").asJson.nospaces should equal("""{"1":"one"}""")
+    Map(2L -> 22222).asJson.nospaces should equal("""{"2":22222}""")
   }
 
   it should "decode lengthy maps" in {
     jObjectFields("1" := "a", "2" := "b")
       .as[Map[Long, String]]
-      .result.right.value should equal (
-        Map(1 -> "a", 2 -> "b")
-      )
+      .result
+      .right
+      .value should equal(
+      Map(1 -> "a", 2 -> "b")
+    )
   }
 
   it should "provide plenary errors" in {
     val error = jObjectFields("adsf" := 1, "booo" := 2, "3" := 3)
       .as[Map[Long, Int]]
-      .result.left.value._1
+      .result
+      .left
+      .value
+      ._1
 
-    error should equal ("unacceptable non-numeric keys: (adsf, booo)")
+    error should equal("unacceptable non-numeric keys: (adsf, booo)")
+  }
+
+  it should "encode instants" in {
+    val textInstant: String = "2018-07-19T13:00:00Z"
+    val instant: Instant    = Instant.parse(textInstant)
+    instant.asJson.toString() shouldEqual s""""$textInstant""""
+  }
+
+  it should "decode instants" in {
+    val textInstant: String = "2018-07-19T13:00:00Z"
+    jString(textInstant).as[Instant].result.right.get shouldBe Instant.parse(textInstant)
   }
 
 }
