5d76c5243ab7558a600d1683d898720af7fa6d0c
merlin <merlin@learningobjects.com>
2018-07-20 08:24:59 -0400

    (e1f0ef2088dde984dc22fc7cc6d28d5ce65697ac)

--- src/main/scala/scaloi/misc/InstantInstances.scala
+++ src/main/scala/scaloi/misc/InstantInstances.scala
@@ -8,10 +8,7 @@ import scalaz._
 trait InstantInstances {
 
   implicit final val instantOrder: Order[Instant] =
-    new Order[Instant] {
-      def order(x: Instant, y: Instant): Ordering =
-        Ordering.fromInt(x compareTo y)
-    }
+    (x: Instant, y: Instant) => Ordering.fromInt(x compareTo y)
 }
 
 object InstantInstances extends InstantInstances
