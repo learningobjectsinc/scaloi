5d219386e9cd260229509483e3f9c2370e9b8bef
merlin <merlin@learningobjects.com>
2018-07-22 11:46:45 -0400

    xmorphism

--- src/main/scala/scaloi/syntax/TreeOps.scala
+++ src/main/scala/scaloi/syntax/TreeOps.scala
@@ -1,7 +1,8 @@
 package scaloi
 package syntax
 
-import scalaz._
+import scalaz.Tree.Node
+import scalaz.{DList, Need, Tree}
 
 final class TreeOps[A](private val self: Tree[A]) extends AnyVal {
 
@@ -19,6 +20,21 @@ final class TreeOps[A](private val self: Tree[A]) extends AnyVal {
     }
 
     loop(self)
+  }
+
+  /** An xmorphism over a tree.
+    *
+    * The tree is mapped with a function `f` that that can draw from
+    * the root label of each node and the mapped values of ancestor
+    * nodes.
+    */
+  def xmorph[B](f: (=> Stream[B], A) => B): Tree[B] = {
+    def loop(tree: Tree[A], ancestors: => Stream[B]): Tree[B] = {
+      val bs = Need(ancestors)
+      lazy val b = f(bs.value, tree.rootLabel)
+      Node(b, tree.subForest.map(loop(_, b #:: bs.value)))
+    }
+    loop(self, Stream.empty)
   }
 
   def get(ix: Int): Option[Tree[A]] = self.subForest.lift.apply(ix)
