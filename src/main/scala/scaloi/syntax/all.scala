8b17b1f80cbdd854d037651254e47195fc81f355
Michael Kalish <mkalish@learningobjects.com>
2018-07-25 18:11:42 +0000

    (96af72de74a2825c602bdbc6d3ee851f618a3ace)

---
+++ src/main/scala/scaloi/syntax/all.scala
@@ -0,0 +1,42 @@
+package scaloi.syntax
+
+/**
+  * @author mkalish
+  */
+
+trait all
+  extends ToAlignOps
+  with ToAnnotationOps
+  with ToAnyOps
+  with ToBooleanOps
+  with ToClassOps
+  with ToClassTagOps
+  with ToCobindOps
+  with ToCollectionBoxOps
+  with ToCollectionOps
+  with ToDateOps
+  with ToDisjunctionOps
+  with ToEitherOps
+  with ToEnumEntryOps
+  with ToFiniteDurationOps
+  with ToFoldableOps
+  with ToFunctorOps
+  with ToHypermonadOps
+  with ToJEnumOps
+  with ToLockOps
+  with ToMapOps
+  with ToMonadOps
+  with ToMutableMapOps
+  with ToOptionOps
+  with ToReadWriteLockOps
+  with ToSeqOps
+  with ToSetOps
+  with ToStringOps
+  with ToTreeOps
+  with ToTryOps
+  with ToTryAnyOps
+  with ToTryCompanionOps
+
+
+
+object all extends all
